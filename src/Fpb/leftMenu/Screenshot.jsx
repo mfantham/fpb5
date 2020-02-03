bf52b797cb8fe20c1838576f33e20bed21e07c3c
Marcus <mfantham@oni.bio>
2020-02-03 22:03:51 +0000

    Basic screenshot functionality

--- src/Fpb/leftMenu/Screenshot.jsx
+++ src/Fpb/leftMenu/Screenshot.jsx
@@ -3,7 +3,14 @@ import styled from "styled-components";
 import Button from "./Button";
 
 const takeScreenshot = () => {
-  console.log("take a picture");
+  const canvas = document.getElementsByTagName("canvas")[0]; // very unreactful :(
+  const link = document.createElement("a");
+  link.download = "screenshot.png";
+
+  canvas.toBlob(blob => {
+    link.href = URL.createObjectURL(blob);
+    link.click();
+  }, "image/png");
 };
 
 export default () => {
